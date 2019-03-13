4119b96c9e61ce310e7c49077af47763dd7ea50c
Alex Lanzano <lanzano.alex@gmail.com>
2019-03-12 23:02:45 -0400

    Fixed syntax issues and mispellings Added networkmanager

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,18 +4,18 @@
 sudo pacman -S --needed - < package.list
 
 # Install AUR packages
-# for url in (AUR.list)
-# do
-# 	git clone $url
-# 	package="$(echo $url | grep -o '[^/]*$')"
-# 	package="$(cut -d "." -f 1 <<< $package)"
-# 	cd $package
-# 	makepkg -si
-# 	cd ..
-# done
+while read url
+do
+	git clone $url
+	package="$(echo $url | grep -o '[^/]*$')"
+	package="$(cut -d "." -f 1 <<< $package)"
+	cd $package
+	makepkg -si
+	cd ..
+done < AUR.list
 
 # Setup configuration files
-rm ~/.xinintrc ~/.bashrc ~/.Xdefaults ~/.emacs.d/init.el
+rm ~/.xinitrc ~/.bashrc ~/.Xdefaults ~/.emacs.d/init.el
 ln xinitrc ~/.xinitrc
 ln bashrc ~/.bashrc
 ln Xdefaults ~/.Xdefaults
@@ -23,4 +23,4 @@ ln init.el ~/.emacs.d/init.el
 
 # Enable Daemons
 systemctl enable --user emacs
-#sudo systemctl enable dhcpcd
+systemctl enable networkmanager
