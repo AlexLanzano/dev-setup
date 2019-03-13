f55ae1de4a3ee7545b0958d4b8b575829c74f27f
Alex Lanzano <lanzano.alex@gmail.com>
2019-03-12 22:48:30 -0400

    Temporary changes

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 
 # Install Packages
-sudo pacman -S - package.list
+sudo pacman -S --needed - < package.list
 
 # Install AUR packages
-for url in (AUR.list)
-do
-	git clone $url
-	package="$(echo $url | grep -o '[^/]*$')"
-	package="$(cut -d "." -f 1 <<< $package)"
-	cd $package
-	makepkg -si
-	cd ..
-done
+# for url in (AUR.list)
+# do
+# 	git clone $url
+# 	package="$(echo $url | grep -o '[^/]*$')"
+# 	package="$(cut -d "." -f 1 <<< $package)"
+# 	cd $package
+# 	makepkg -si
+# 	cd ..
+# done
 
 # Setup configuration files
+rm ~/.xinintrc ~/.bashrc ~/.Xdefaults ~/.emacs.d/init.el
 ln xinitrc ~/.xinitrc
 ln bashrc ~/.bashrc
 ln Xdefaults ~/.Xdefaults
@@ -22,4 +23,4 @@ ln init.el ~/.emacs.d/init.el
 
 # Enable Daemons
 systemctl enable --user emacs
-sudo systemctl enable dhcpcd
+#sudo systemctl enable dhcpcd
